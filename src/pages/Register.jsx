b3da622e1f451c313e3f82d37f765fbd3b88cbaf
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-07-06 10:22:16 +0530

    added new changes

---
+++ src/pages/Register.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <>
+        {/* Section: Design Block */}
+        <section className="">
+          {/* Jumbotron */}
+          <div
+            className="px-4 py-5 px-md-5 text-center h-100vh text-lg-start"
+            style={{ backgroundColor: "hsl(0, 0%, 96%)" }}
+          >
+            <div className="container">
+              <div className="row gx-lg-5 align-items-center">
+                <div className="col-lg-6 mb-5 mb-lg-0">
+                  <h1 className="my-5 display-3 fw-bold ls-tight">
+                    Todo List <br />
+                    <span className="text-primary">Manage your time</span>
+                  </h1>
+                  <p style={{ color: "hsl(217, 10%, 50.8%)" }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+                    itaque accusantium odio, soluta, corrupti aliquam quibusdam
+                    tempora at cupiditate quis eum maiores libero veritatis? Dicta
+                    facilis sint aliquid ipsum atque?
+                  </p>
+                </div>
+                <div className="col-lg-6 mb-5 mb-lg-0">
+                  <div className="card">
+                    <div className="card-body py-5 px-md-5">
+                      <form>
+                        {/* 2 column grid layout with text inputs for the first and last names */}
+                        <div className="row">
+                          <div className="col-md-6 mb-4">
+                            <div data-mdb-input-init="" className="form-outline">
+                              <input
+                                type="text"
+                                id="form3Example1"
+                                className="form-control"
+                              />
+                              <label className="form-label" htmlFor="form3Example1">
+                                First name
+                              </label>
+                            </div>
+                          </div>
+                          <div className="col-md-6 mb-4">
+                            <div data-mdb-input-init="" className="form-outline">
+                              <input
+                                type="text"
+                                id="form3Example2"
+                                className="form-control"
+                              />
+                              <label className="form-label" htmlFor="form3Example2">
+                                Last name
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                        {/* Email input */}
+                        <div data-mdb-input-init="" className="form-outline mb-4">
+                          <input
+                            type="email"
+                            id="form3Example3"
+                            className="form-control"
+                          />
+                          <label className="form-label" htmlFor="form3Example3">
+                            Email address
+                          </label>
+                        </div>
+                        {/* Password input */}
+                        <div data-mdb-input-init="" className="form-outline mb-4">
+                          <input
+                            type="password"
+                            id="form3Example4"
+                            className="form-control"
+                          />
+                          <label className="form-label" htmlFor="form3Example4">
+                            Password
+                          </label>
+                        </div>
+                        {/* Checkbox */}
+                        <div className="form-check d-flex justify-content-center mb-4">
+                          <input
+                            className="form-check-input me-2"
+                            type="checkbox"
+                            defaultValue=""
+                            id="form2Example33"
+                            defaultChecked=""
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="form2Example33"
+                          >
+                            Subscribe to our newsletter
+                          </label>
+                        </div>
+                        {/* Submit button */}
+                        <button
+                          type="submit"
+                          data-mdb-button-init=""
+                          data-mdb-ripple-init=""
+                          className="btn btn-primary btn-block mb-4"
+                        >
+                          Sign up
+                        </button>
+
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Jumbotron */}
+        </section>
+        {/* Section: Design Block */}
+      </>
+
+    </div>
+  )
+}
+
+export default Register
